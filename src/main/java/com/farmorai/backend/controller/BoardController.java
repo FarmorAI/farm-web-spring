482e40b1e6b270a5a72544ee552f6f2ded4b1ab4
dlekgp1183 <dlekgp1183@naver.com>
2025-02-12 19:15:26 +0900

    commit

--- src/main/java/com/farmorai/backend/controller/BoardController.java
+++ src/main/java/com/farmorai/backend/controller/BoardController.java
@@ -1,6 +1,10 @@
 package com.farmorai.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BoardController {
-
+    @GetMapping
+    public void hello() {}
 }
