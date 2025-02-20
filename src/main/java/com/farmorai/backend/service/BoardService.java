3a676abceaaf7a23e87945932ef503a1e3e954c9
dlekgp1183 <dlekgp1183@naver.com>
2025-02-20 18:28:26 +0900

    board file upload

--- src/main/java/com/farmorai/backend/service/BoardService.java
+++ src/main/java/com/farmorai/backend/service/BoardService.java
@@ -2,9 +2,11 @@ package com.farmorai.backend.service;
 
 import com.farmorai.backend.dto.BoardDto;
 import com.farmorai.backend.mapper.BoardMapper;
+import com.farmorai.backend.util.FileUploadUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardMapper boardMapper;
+    private final FileUploadUtil fileUploadUtil;
 
     public List<BoardDto> getBoardList(){
         return boardMapper.getBoardList();
@@ -36,5 +39,9 @@ public class BoardService {
     public BoardDto getBoardDetail(Long boardId) {
         boardMapper.updateBoardViews(boardId);
         return boardMapper.getBoardDetail(boardId);
+    }
+
+    public List<String> saveBoardFiles(List<MultipartFile> files, String uploadDir){
+        return fileUploadUtil.saveFiles(files);
     }
 }
