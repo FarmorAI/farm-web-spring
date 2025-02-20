6beb3ea46df1aa78e6153e1e3c7073b41256b602
hogi2020 <junmyung2020@gmail.com>
2025-02-20 23:28:00 +0900

    payment update

--- src/main/java/com/farmorai/backend/mapper/PaymentMapper.java
+++ src/main/java/com/farmorai/backend/mapper/PaymentMapper.java
@@ -4,10 +4,13 @@ import com.farmorai.backend.dto.PaymentDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PaymentMapper {
     List<PaymentDto> getAllPayment();
 
     void insertPayment(PaymentDto paymentDto);
+
+    List<PaymentDto> getPaymentById(PaymentDto paymentDto);
 }
