458f57a9b3f0a193ead5e7df92ae262c2b90adbe
hogi2020 <junmyung2020@gmail.com>
2025-02-21 19:21:26 +0900

    update payment sql query

--- src/main/java/com/farmorai/backend/controller/PaymentController.java
+++ src/main/java/com/farmorai/backend/controller/PaymentController.java
@@ -2,7 +2,9 @@ package com.farmorai.backend.controller;
 
 import com.farmorai.backend.dto.NaverPayInfoDto;
 import com.farmorai.backend.dto.PaymentDto;
+import com.farmorai.backend.dto.SubsDto;
 import com.farmorai.backend.service.PaymentService;
+import com.farmorai.backend.service.SubsService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,20 +18,24 @@ import reactor.core.publisher.Mono;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/payment")
 public class PaymentController {
-    private final WebClient webClient;
+    private WebClient webClient;
     private final PaymentService paymentService;
+    private final SubsService subsService;
 
     @Autowired
-    public PaymentController(PaymentService paymentService, WebClient.Builder webClientBuilder) {
+    public PaymentController(WebClient.Builder webClientBuilder, PaymentService paymentService, SubsService subsService) {
         this.webClient = webClientBuilder.baseUrl("https://dev-pub.apis.naver.com").build();
         this.paymentService = paymentService;
+        this.subsService = subsService;
     }
 
     @Value("${naver.pay.client-id}")
@@ -47,13 +53,15 @@ public class PaymentController {
         String subsPlan = reqBody.get("subsPlan");
         String subsPrice = reqBody.get("subsPrice");
 
-        // Validate inputs
+        // Validate Params
         if (subsPlan == null || subsPrice == null) {
             return Mono.fromSupplier(() -> ResponseEntity.badRequest().body("Missing required parameters"));
         }
 
-        // paymentDto 객체 생성
-        PaymentDto paymentDto = paymentService.insertPayment("admin@admin.com", subsPlan, subsPrice);
+        // PaymentDto 생성 및 레코드 DB 저장
+        PaymentDto paymentDto = paymentService.insertPayment("admin1@example.com", subsPlan, subsPrice);
+        String pid = "pid" + paymentDto.getPaymentId();
+        String encodedPid = Base64.getEncoder().encodeToString(pid.getBytes(StandardCharsets.UTF_8));
 
         // NaverPay API 요청 정보
         NaverPayInfoDto payInfo = NaverPayInfoDto.builder()
@@ -63,10 +71,8 @@ public class PaymentController {
                 .totalPayAmount(Integer.parseInt(subsPrice))
                 .taxScopeAmount(Integer.parseInt(subsPrice))
                 .taxExScopeAmount(0)
-                .returnUrl(String.format("http://localhost:6060/payment/result?subsPlan=%s&subsPrice=%s", subsPlan, subsPrice))
+                .returnUrl(String.format("http://localhost:6060/payment/result?subsPlan=%s&subsPrice=%s&epd=%s", subsPlan, subsPrice, encodedPid))
                 .build();
-
-        log.debug("payInfo : {}", payInfo);
 
         return webClient.post()
                 .uri("/naverpay-partner/naverpay/payments/v2/reserve")
@@ -90,16 +96,26 @@ public class PaymentController {
             @RequestParam(required = false) String paymentId,
             @RequestParam String subsPlan,
             @RequestParam String subsPrice,
+            @RequestParam(required = false) String epd,
             HttpServletResponse resp
     ) throws IOException {
 
-        // URL 인코딩 처리
+        // encodedPid decoding
+        String decodedpid = new String(Base64.getDecoder().decode(epd));
+        Long pid = Long.parseLong(decodedpid.substring(3));
+
+        // create SubDto and update paymentDto
+        PaymentDto paymentDto = paymentService.getPaymentById(pid);
+        SubsDto subsDto = subsService.insertSubs(paymentDto.getMemberId(), paymentDto.getPlanId());
+        paymentService.updatePayment(paymentDto, paymentId, subsDto.getSubsId());
+
+        // string encoding
         String encodedSubsPlan = URLEncoder.encode(subsPlan, StandardCharsets.UTF_8);
         String redirectUrl = "http://localhost:3030/payment/result?";
         String params = "&resultCode=" + resultCode + "&paymentId=" + paymentId + "&subsPlan=" + encodedSubsPlan + "&subsPrice=" + subsPrice;
 
-        // 결과 코드가 Success이면 성공 페이지로 리다이렉트
         if ("Success".equals(resultCode)) {
+            // 결과 코드가 Success이면 성공 페이지로 리다이렉트
             resp.sendRedirect(redirectUrl + params);
         } else {
             // 실패한 경우 실패 페이지로 리다이렉트
@@ -112,8 +128,8 @@ public class PaymentController {
     public Map<String, String> naverPayCancel(@RequestBody Map<String, String> reqBody) {
         String paymentId = reqBody.get("paymentId");
 
-        log.debug("cancel paymentId : {}", paymentId);
-
+        subsService.cancelSubs(paymentId);
+        paymentService.refundPayment(paymentId);
         return Map.of("result", paymentId);
     }
 }
--- src/main/java/com/farmorai/backend/service/PaymentService.java
+++ src/main/java/com/farmorai/backend/service/PaymentService.java
@@ -6,30 +6,51 @@ import com.farmorai.backend.mapper.PaymentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
     private final PaymentMapper paymentMapper;
     private final MemberMapper memberMapper;
 
-    public PaymentDto insertPayment(String email, String subPlan, String subPrice) {
-        // memberDto
-        MemberDto memberDto = memberMapper.getMemberByEmail(email);
+    // PaymentDto 조회
+    public PaymentDto getPaymentById(Long paymentId) {
+        PaymentDto paymentDto = paymentMapper.getPaymentById(paymentId);
+        return paymentDto;
+    }
 
-        // paymentDto
+    // PENDING : PaymentDto 생성 및 레코드 저장
+    public PaymentDto insertPayment(String email, String subsPlan, String subsPrice) {
+        MemberDto memberDto = memberMapper.getMemberByEmail(email);
+        Long planId = (long) PaymentPlan.valueOf(subsPlan).ordinal();
+
         PaymentDto paymentDto = new PaymentDto(
                 null,
-                Integer.parseInt(subPrice),
+                Integer.parseInt(subsPrice),
                 null,
                 null,
                 PaymentMethod.NAVERPAY,
                 PaymentStatus.PENDING,
-                Long.parseLong(String.valueOf(subPlan)),
+                planId,
                 memberDto.getMemberId(),
                 null
         );
 
         paymentMapper.insertPayment(paymentDto);
         return paymentDto;
+    }
+
+    // Payment 레코드 저장 (Completed)
+    public void updatePayment(PaymentDto paymentDto, String paymentId, Long subsId) {
+        paymentDto.setToken(paymentId);
+        paymentDto.setStatus(PaymentStatus.COMPLETED);
+        paymentDto.setSubsId(subsId);
+        paymentMapper.updatePayment(paymentDto);
+    }
+
+    // Payment Update
+    public void refundPayment(String token) {
+        paymentMapper.refundPayment(token);
     }
 }
