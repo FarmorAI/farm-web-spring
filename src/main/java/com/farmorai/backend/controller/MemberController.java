df6a9037a4177f2068425b29f168d59de8a669a4
hogi2020 <junmyung2020@gmail.com>
2025-02-24 11:11:59 +0900

    fixed join and login function

--- src/main/java/com/farmorai/backend/controller/MemberController.java
+++ src/main/java/com/farmorai/backend/controller/MemberController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
